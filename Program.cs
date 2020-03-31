dfcf8d16b2d3655d187157e3244706b39dcf3a04
Josué Alejandro Díaz Rojas <Patchinster@Gmail.com>
2020-03-31 06:30:33 -0600

    Realización de la tarea

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-
 namespace SocketCom
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if(args == null)
+            {
+                Console.WriteLine("null");
+            }
             if(args[0] == "server")
             {
                 TCPServer server = new TCPServer("127.0.0.1", 8080, true);
                 server.Listen();
             } else if(args[0] == "client")
             {
-                string name = "Cliente";
-                try
-                {
-                    name = args[1];
-                }
-                catch (Exception) { }
-
-                // Ejecución de cliente .NET Core
+                TCPClient cliente = new TCPClient();
+                cliente.Connect("127.0.0.1", 8080);
+                while (true){
+                    cliente.client.GetStream();
+                    Console.WriteLine(cliente.ReadMessage());
+                    cliente.SendMessage(Console.ReadLine());
+                };
             }
         }
     }
---
+++ TCPClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketCom
+{
+	class TCPClient
+	{
+		public TcpClient client { get; set; }
+		public NetworkStream stream { get; set; }
+		public TCPClient()
+		{
+			client = new TcpClient();
+		}
+
+		public void Connect(string ip, int port)
+		{
+			IPAddress address = IPAddress.Parse(ip);
+			client.Connect(address, port);
+			stream = client.GetStream();
+		}
+
+		public void SendMessage(string message)
+		{
+			byte[] data = Encoding.UTF8.GetBytes(message);
+			stream.Write(data);
+		}
+
+		public string ReadMessage()
+		{
+			byte[] buffer = new byte[1024];
+			stream.Read(buffer);
+			string resultado  = Encoding.ASCII.GetString(buffer);
+			return resultado;
+		}
+	}
+}
